5ff8776c200158bb5c56c86981b16e7641b0eeab
PhamHung2020 <phammanhhung10501@gmail.com>
2024-02-28 09:03:11 +0700

    Add some code

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-# This is a sample Python script.
+from datasets import load_dataset, load_from_disk
+import csv
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+difficulties = ['EASY']
+
+# Load train dataset from Hugging Face
+# taco = load_dataset('BAAI/TACO', token='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', split='train', trust_remote_code=True)
+# taco.save_to_disk("dataset/train")
+
+# Load train dataset from disk
+taco_from_disk = load_from_disk("dataset/train")
+taco = taco_from_disk.filter(lambda entry: entry['difficulty'] in difficulties)
+
+taco_10 = taco.select(range(10))
+print(taco_10.column_names)
+print(taco_10["input_output"][1])
+# print(taco_10['question'][0])
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# taco_10.to_csv("dataset/train_easy_dataset.csv")
+#
+# print(taco.column_names)
+# print(taco.description)
+# print(taco.data.schema)
+with open("question.txt", "w") as of:
+    for i in range(10):
+        of.write(f"{i}\n")
+        of.write(bytes(taco[i]['question'], 'utf-8').decode('unicode_escape'))
+        of.write(f"\nEnd of {i}\n\n\n\n")
